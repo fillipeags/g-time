b5deba9f425c38d0ca1f3e4c4af11e40d91a6d80
Fillipe <fillipe484@gmail.com>
2021-12-04 22:30:17 -0300

    feat: search small cards components

---
+++ src/utils/swiper-settings.ts
@@ -0,0 +1,12 @@
+// swiper core styles
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/swiper.min.css';
+// modules styles
+import 'swiper/components/navigation/navigation.min.css';
+import 'swiper/components/pagination/pagination.min.css';
+
+// import Swiper core and required modules
+import SwiperCore, { Pagination, Navigation } from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Pagination, Navigation]);
