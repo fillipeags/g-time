3178524e02ca8d94055bbab323a50c11f1b673c1
Fillipe <fillipe484@gmail.com>
2022-01-08 14:09:56 -0300

    fix: sidebar alignment

--- src/components/SideBar/styles.ts
+++ src/components/SideBar/styles.ts
@@ -15,6 +15,7 @@ export const Container = styled.div<MenuItemProps>`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
+  align-items: center;
 
   transition: 0.3s ease-in;
 `;
