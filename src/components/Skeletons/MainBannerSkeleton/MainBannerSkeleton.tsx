b9c0f1634f46142b6ba5b044995cedcf9f653c6d
Fillipe <fillipe484@gmail.com>
2022-01-13 08:18:25 -0300

    feat: mainBannerSkeleton

--- src/components/Skeletons/MainBannerSkeleton/MainBannerSkeleton.tsx
+++ src/components/Skeletons/MainBannerSkeleton/MainBannerSkeleton.tsx
@@ -1,20 +1,25 @@
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 import { Container, Wrapper, Content } from '../../MainBanner/styles';
 
 const MainBannerSkeleton = () => {
   return (
-    <SkeletonTheme baseColor="#ffffff" highlightColor="#aca9a9">
+    <SkeletonTheme baseColor="#fafafac7" highlightColor="#ffffff">
       <Container>
         <Content>
-          <Skeleton width="200px" height={20} baseColor="white" />
+          <Skeleton width="600px" height={50} />
 
           <Wrapper>
-            <Skeleton count={3} />
+            <Skeleton width="350px" height={50} />
             <div>
               <Skeleton height={30} />
             </div>
-            <Skeleton width="200px" height={20} />
+            <Skeleton
+              width="200px"
+              height={50}
+              style={{ marginRight: ' 40px' }}
+            />
           </Wrapper>
         </Content>
       </Container>
