2f154d8fcef020de4be8fca346b5d7d83d9414b3
Fillipe <fillipe484@gmail.com>
2022-01-16 16:33:16 -0300

    refactor: largeCard and largeCardItem components types and api fetch

--- src/components/Cards/LargeCard/index.tsx
+++ src/components/Cards/LargeCard/index.tsx
@@ -1,23 +1,46 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useEffect, useState } from 'react';
 import breakpoints from '../../../utils/swiper-settings';
 
 import LargeCardItem from './LargeCardItem';
 import { CardsContainer } from './styles';
 
-import data from '../../../database/mock';
+import IGamesApiDTO from '../../../dtos/apiDTO';
+import api from '../../../services/api';
+import requests from '../../../services/api/requests';
+import ErrorHandler from '../../../helpers/Toast/Error';
 
 const LargeCard = () => {
+  const [games, setGames] = useState<IGamesApiDTO[]>([]);
+
+  useEffect(() => {
+    async function fetchGames() {
+      try {
+        setGames([]);
+        const res = await api.get(requests.popularGames);
+        setGames(res.data.results);
+
+        // eslint-disable-next-line no-console
+        console.log(res);
+      } catch (error) {
+        ErrorHandler('Oops, Something Went Wrong in our Servers');
+      }
+    }
+
+    fetchGames();
+  }, []);
+
   return (
     <CardsContainer>
       <h1>Best Scored Games</h1>
       <h3>The best games to be played according to the community</h3>
       <Swiper slidesPerView={3} spaceBetween={800} breakpoints={breakpoints}>
-        {data.map(({ id, title, score, coverImage }) => (
+        {games.map(({ id, name, rating, background_image }) => (
           <SwiperSlide key={id}>
             <LargeCardItem
-              coverImage={coverImage}
-              title={title}
-              score={score}
+              background_image={background_image}
+              name={name}
+              rating={rating}
             />
           </SwiperSlide>
         ))}
